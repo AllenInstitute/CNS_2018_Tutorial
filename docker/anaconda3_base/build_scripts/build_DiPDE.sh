0c27e2ecd99d00c492bf8169ef028e16e0219c94
kael <kael.dai@gmail.com>
2018-07-06 15:45:25 -0700

    chaning bmtk env from 3.6 to 2.7

--- docker/anaconda3_base/build_scripts/build_DiPDE.sh
+++ docker/anaconda3_base/build_scripts/build_DiPDE.sh
@@ -4,6 +4,8 @@ USE_CONDA=true
 CONDA_ENV=''
 BUILD_PATH=/home/build
 INSTALL_PATH=/home/shared
+PYTHON_VER='python2.7'
+PACKAGE_STR=''
 
 # Check scripts options
 while [[ $# -gt 0 ]]; do
@@ -12,6 +14,7 @@ while [[ $# -gt 0 ]]; do
 		-e|--conda-env) CONDA_ENV=$2; shift ;;
 		-b|--build-path) BUILD_PATH=$2; shift ;;
 		-i|"--install-path") INSTALL_PATH=$2; shift ;;
+		-p|--python-ver) PACKAGE_STR=PYTHON_VER; shift ;;
 	esac
 	shift
 done
@@ -21,9 +24,13 @@ if [[ ! -z ${CONDA_ENV} ]]; then
 	source activate ${CONDA_ENV}
 fi
 
+if [[ ${PYTHON_VER} == "3*" ]]; then
+	PACKAGE_STR='=py36_3'
+fi
+
 if [[ ${USE_CONDA}=true ]]; then
 	# install using conda
-	conda install -y -c nicholasc dipde=0.2.1=py36_3
+	conda install -y -c nicholasc dipde=0.2.1${PACKAGE_STR}
 else
 	# TODO: get and build latest version of Dipde from source
 	echo "ERROR: At the moment DiPDE can only be installed through conda"
